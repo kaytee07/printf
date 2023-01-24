8152a3e2518162bc16e4b39805e348cd8393bb6e
Winful KOfi Taylor <kofi.taylor07@gmail.com>
2023-01-24 06:28:47 +0000

    correcting varibale errors

--- _printf.c
+++ _printf.c
@@ -6,14 +6,15 @@
  *
  * Return: number of bytes printed
  */
+
 int _printf(const char *format, ...)
 {
-int sum = 0;
-va_list ap;
+int dum = 0;
+va_list list;
 char *p, *start;
 params_t params = PARAMS_INIT;
 
-va_start(ap, format);
+va_start(list, format);
 
 if (!format || (format[0] == '%' && !format[1]))
 return (-1);
@@ -21,10 +22,10 @@ if (format[0] == '%' && format[1] == ' ' && !format[2])
 return (-1);
 for (p = (char *)format; *p; p++)
 {
-init_params(&params, ap);
+init_params(&params, list);
 if (*p != '%')
 {
-sum += _putchar(*p);
+dum += _putchar(*p);
 continue;
 }
 start = p;
@@ -33,18 +34,18 @@ while (get_flag(p, &params))
 {
 p++;
 }
-p = get_width(p, &params, ap);
-p = get_precision(p, &params, ap);
+p = get_width(p, &params, list);
+p = get_precision(p, &params, list);
 if (get_modifier(p, &params))
 p++;
 if (!get_specifier(p))
-sum += print_from_to(start, p,
+dum += print_from_to(start, p,
 params.l_modifier || params.h_modifier ? p - 1 : 0);
- else
-sum += get_print_func(p, ap, &params);
+else
+dum += get_print_func(p, list, &params);
 }
 _putchar(BUF_FLUSH);
-va_end(ap);
-return (sum);
+va_end(list);
+return (dum);
 }
 
